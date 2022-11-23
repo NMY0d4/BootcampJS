96782cba67b188e25f5e3ce381a6d55c4508b07e
Grégory Marini <greg.marini@hotmail.fr>
2022-11-23 19:17:31 +0100

    work on player movement

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 function isTouching(a, b) {
-	const aRect = a.getBoundingClientRect();
-	const bRect = b.getBoundingClientRect();
+    const aRect = a.getBoundingClientRect();
+    const bRect = b.getBoundingClientRect();
 
-	return !(
-		aRect.top + aRect.height < bRect.top ||
-		aRect.top > bRect.top + bRect.height ||
-		aRect.left + aRect.width < bRect.left ||
-		aRect.left > bRect.left + bRect.width
-	);
+    return !(
+        aRect.top + aRect.height < bRect.top ||
+        aRect.top > bRect.top + bRect.height ||
+        aRect.left + aRect.width < bRect.left ||
+        aRect.left > bRect.left + bRect.width
+    );
 }
+
+const avatar = document.querySelector("#player");
+const extractPos = (pos) => {
+    if (!pos) return 0;
+    return +pos.slice(0, -2);
+};
+
+window.addEventListener("keyup", function (e) {
+    if (e.key === "ArrowDown") {
+        const currTop = extractPos(avatar.style.top);
+        avatar.style.top = `${currTop + 50}px`;
+    }
+});
