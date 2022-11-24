c3a71d0b49d2d059663501fe8cd33d492c18ce31
Grégory Marini <greg.marini@hotmail.fr>
2022-11-24 09:55:57 +0100

    refactoring with a move function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function isTouching(a, b) {
     const aRect = a.getBoundingClientRect();
     const bRect = b.getBoundingClientRect();
+    // const currTop = 0;
 
     return !(
         aRect.top + aRect.height < bRect.top ||
@@ -11,26 +12,27 @@ function isTouching(a, b) {
 }
 
 const avatar = document.querySelector("#player");
-
-window.addEventListener("keyup", function (e) {
-    if (e.key === "ArrowDown" || e.key === "Down") {
-        const currTop = extractPos(avatar.style.top);
-        avatar.style.top = `${currTop + 50}px`;
-    } else if (e.key === "ArrowUp" || e.key === "Up") {
-        const currTop = extractPos(avatar.style.top);
-        avatar.style.top = `${currTop - 50}px`;
-    } else if (e.key === "ArrowRight" || e.key === "Right") {
-        const currLeft = extractPos(avatar.style.left);
-        avatar.style.left = `${currLeft + 50}px`;
-        avatar.style.transform = "scale(1,1)";
-    } else if (e.key === "ArrowLeft" || e.key === "Left") {
-        const currLeft = extractPos(avatar.style.left);
-        avatar.style.left = `${currLeft - 50}px`;
-        avatar.style.transform = "scale(-1,1)";
-    }
-});
+avatar.style.top = "100px";
+avatar.style.left = "100px";
 
 const extractPos = (pos) => {
     if (!pos) return 0;
     return parseInt(pos.slice(0, -2));
 };
+const move = (pos, speed, scale) => {
+    if (scale) avatar.style.transform = `scale(${scale},1)`;
+    return (pos = `${extractPos(pos) + speed}px`);
+};
+
+window.addEventListener("keyup", function (e) {
+    const avSt = avatar.style;
+    if (e.key === "ArrowDown" || e.key === "Down") {
+        avSt.top = move(avSt.top, 50);
+    } else if (e.key === "ArrowUp" || e.key === "Up") {
+        avSt.top = move(avSt.top, -50);
+    } else if (e.key === "ArrowRight" || e.key === "Right") {
+        avSt.left = move(avSt.left, 50, 1);
+    } else if (e.key === "ArrowLeft" || e.key === "Left") {
+        avSt.left = move(avSt.left, -50, -1);
+    }
+});
