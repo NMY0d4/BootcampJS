80d39c9bc9feddb5710d10de04df67d34faba3d4
Grégory Marini <greg.marini@hotmail.fr>
2022-12-21 17:23:56 +0100

    create code for see if is file or not... but bad solution

--- node/list/index.js
+++ node/list/index.js
@@ -7,5 +7,14 @@ fs.readdir(process.cwd(), (err, filenames) => {
         console.error(err);
     }
 
-    console.log(filenames);
+    // BAD CODE HERE!!!
+    for (let filename of filenames) {
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(filename, stats.isFile());
+        });
+    }
+    // BAD CODE COMPLETE
 });
